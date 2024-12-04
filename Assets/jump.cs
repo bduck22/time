6a258c1f51e8fedae0975ec43bdf7f51c0f246b3
myagmb28Dev <myagmb28s@gmail.com>
2024-12-04 18:07:14 +0900

    Revert "ui, jump cooldown"
    This reverts commit 130e57b011a14da68a75b54d0234399a8c75e563.

--- Assets/jump.cs
+++ Assets/jump.cs
@@ -1,23 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
-<<<<<<< Updated upstream
 using Unity.Burst.CompilerServices;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Timeline;
 using static UnityEditor.PlayerSettings;
-=======
-using UnityEngine;
->>>>>>> Stashed changes
 
 public class jump : MonoBehaviour
 {
-    public CooldownUi coolBar;
     bool onejump;
     Rigidbody2D body;
     public int power;
     Animator ani;
-<<<<<<< Updated upstream
     GameMaster master;
     bool stop;
     bool slide;
@@ -26,23 +20,12 @@ public class jump : MonoBehaviour
     {
         once = true;
         master = GameObject.FindWithTag("master").GetComponent<GameMaster>();
-=======
-    public float cooldownTime = 2f;
-    private float currentCooldownTime;
-
-    void Start()
-    {
-        coolBar.per.fillAmount = 1;
->>>>>>> Stashed changes
         onejump = true;
         body = GetComponent<Rigidbody2D>();
         ani = GetComponent<Animator>();
-        currentCooldownTime = cooldownTime;
     }
-
     void Update()
     {
-<<<<<<< Updated upstream
         if (!stop) { 
             if (((Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.UpArrow)) )&& onejump&&!Input.GetKey(KeyCode.DownArrow))
             {
@@ -64,28 +47,10 @@ public class jump : MonoBehaviour
                 ani.SetBool("Slide", false);
                 GetComponent<BoxCollider2D>().enabled = false;
                 GetComponent<CapsuleCollider2D>().enabled = true;
-=======
-        if ((Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.UpArrow)) && onejump && coolBar.per.fillAmount == 1)
-        {
-            ani.SetTrigger("Jump");
-            onejump = false;
-            StartCoroutine(Jump());
-            coolBar.per.fillAmount -= 0.1f;
-        }
-
-        if (coolBar.per.fillAmount < 1)
-        {
-            currentCooldownTime -= Time.deltaTime;
-            coolBar.per.fillAmount = Mathf.Lerp(0, 1, (cooldownTime - currentCooldownTime) / cooldownTime);
-            if (currentCooldownTime <= 0)
-            {
-                currentCooldownTime = cooldownTime;
->>>>>>> Stashed changes
             }
         }
         if (onejump && transform.position.y != -5.65f && !slide) transform.position = new Vector3(transform.position.x, -5.65f, transform.position.z);
     }
-
     IEnumerator Jump()
     {
         yield return new WaitForSeconds(0.15f);
@@ -95,10 +60,8 @@ public class jump : MonoBehaviour
             body.AddForce(Vector2.up * power);
         }
     }
-
     private void OnCollisionEnter2D(Collision2D collision)
     {
-<<<<<<< Updated upstream
         if(collision.transform == Physics2D.Raycast(transform.position, Vector2.down))
         {
             ani.SetTrigger("lend");
@@ -140,14 +103,11 @@ public class jump : MonoBehaviour
         yield return new WaitForSeconds(cool);
         Time.timeScale = 1;
     }
-=======
-        ani.SetTrigger("lend");
-        onejump = true;
-    }
-
->>>>>>> Stashed changes
     private void OnCollisionExit2D(Collision2D collision)
     {
-        ani.ResetTrigger("lend");
+        if (collision.transform == Physics2D.Raycast(transform.position, Vector2.down))
+        {
+            ani.ResetTrigger("lend");
+        }
     }
 }
